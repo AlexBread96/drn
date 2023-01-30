7e7a94b9fac314134f998988674f1d7b7bf3eb00
Alexandr <63932653+AlexBread96@users.noreply.github.com>
2023-01-30 16:28:15 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -166,7 +166,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     // тиндер эмитация
     [...empty].forEach((el) => {
-      // el.addEventListener('') 
+      // el.addEventListener('')
       if (
         drag.getBoundingClientRect().top + drag.offsetHeight / 2 <
           el.getBoundingClientRect().bottom &&
@@ -178,7 +178,7 @@ document.addEventListener("DOMContentLoaded", function () {
           el.getBoundingClientRect().right
       ) {
         //   el.classList.add("active");
-       
+
         lastItem = el;
       } else {
         //   el.classList.remove("active");
@@ -200,7 +200,7 @@ document.addEventListener("DOMContentLoaded", function () {
         : null;
       this.style.left = `${lastItem.offsetLeft}px`;
       drag.style.transform = `rotate(0deg)`;
-  
+      setTimeout(() => {
         lastItem.append(this);
         this.remove();
         zone.lastElementChild.setAttribute("id", "drag");
@@ -222,7 +222,7 @@ document.addEventListener("DOMContentLoaded", function () {
           //   console.log(x, y);
           //   console.log(drag.getBoundingClientRect());
         });
-    
+      }, 300);
     } else {
       this.style.transitionProperty = "all";
       this.style.top = `${zone.offsetTop}px`;
